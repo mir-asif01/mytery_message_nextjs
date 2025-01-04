e220787c773af88badca9259406ad067498fdeec
mir-asif01 <asifbsb2002@gmail.com>
2025-01-04 13:39:31 +0600

    Add session and JWT callbacks to include user details in NextAuth

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,18 @@
+import "next-auth"
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      _id?: string
+      isVerified?: boolean
+      isAcceptingMessage?: boolean
+      username?: string
+    } & DefaultSession["user"]
+  }
+  interface User {
+    _id?: string
+    isVerified?: boolean
+    isAcceptingMessage?: boolean
+    username?: string
+  }
+}
